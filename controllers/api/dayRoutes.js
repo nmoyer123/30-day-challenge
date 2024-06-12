0aeba21b62421df16cb07a28e714989316f6bb6b
DMendoza013 <damien.mendoza013@gmail.com>
2024-06-11 20:28:20 -0700

    added routes for login and signup

---
+++ controllers/api/dayRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { User, Day } = require('../../models');
+
+// router.post('/', async (req, res) => {
+//     try{
+//         const challengeData = await Day.create({
+//             ...req.body,
+//         })
+//     }
+// })
+
+module.exports = router;
