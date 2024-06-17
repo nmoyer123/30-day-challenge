a1c2ab446e6d68dc921d934ea6be9d3d406004bd
DMendoza013 <damien.mendoza013@gmail.com>
2024-06-16 19:13:07 -0700

    routes work!

--- public/js/Daybars.js
+++ public/js/Daybars.js
@@ -1,27 +1,17 @@
   const newFormHandler = async (event) => {
     event.preventDefault();
 
-      // const formData = new FormData(form);
-      // const data = {
-      //     run_check: formData.get('run_check') === 'on',
-      //     water_check: formData.get('water_check'),
-      //     page_check: formData.get('page_check'),
-      //     alcohol_check: formData.get('alcohol_check') === 'on',
-      //     workout_check: formData.get('workout_check'),
-      //     sleep_check: formData.get('sleep_check')
-      // };
+      const run_check = document.querySelector('#run-check').value.trim();
+      const water_check = document.querySelector('#water-check').value.trim();
+      const page_check = document.querySelector('#page-check').value.trim();
+      const alcohol_check = document.querySelector('#alcohol-check').value.trim();
+      const workout_check = document.querySelector('#workout-check').value.trim();
+      const sleep_check = document.querySelector('#sleep-check').value.trim();
 
-      const runCheck = document.querySelector('#run-check').value.trim();
-      const waterCheck = document.querySelector('#water-check').value.trim();
-      const pageCheck = document.querySelector('#page-check').value.trim();
-      const alcoholCheck = document.querySelector('#alcohol-check').value.trim();
-      const workoutCheck = document.querySelector('#workout-check').value.trim();
-      const sleepCheck = document.querySelector('#sleep-check').value.trim();
-
-      if (runCheck && waterCheck && pageCheck && alcoholCheck && workoutCheck && sleepCheck) {
+      if (run_check && water_check && page_check && alcohol_check && workout_check && sleep_check) {
         const response = await fetch(`/api/day`, {
           method: 'POST',
-          body: JSON.stringify({runCheck, waterCheck, pageCheck, alcoholCheck, workoutCheck, sleepCheck}),
+          body: JSON.stringify({run_check, water_check, page_check, alcohol_check, workout_check, sleep_check}),
           headers: {
             'Content-Type': 'application/json',
           },
